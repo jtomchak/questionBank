cb06b858f7f00559d8c31eb9325934b2314bae43
Jesse Tomchak <jtomchak@gmail.com>
2017-07-10 09:55:42 -0700

    done town

--- src/api/question/question.api.js
+++ src/api/question/question.api.js
@@ -6,7 +6,7 @@ exports.questionsByAuthor = function(req, res) {
     reqAuthor.charAt(0).toUpperCase() + reqAuthor.slice(1).toLowerCase();
 
   Question.find({ author: reqAuthor })
-    .sort({ createdAt: "asc" })
+    .sort({ createdAt: "desc" })
     .limit(25)
     .exec(function(err, questions) {
       if (err) return next(err);
